770505fa471875e7aefc010bf0c2e71400531635
Tao Peter Wang <peterwangtao0@hotmail.com>
2015-08-20 20:59:54 -0400

    Face-Detective Version 1.0.0.0
    First debug release v1.0.0.0
    
    Added Functions:
    - Retrieve photos from gallery and analyse.
    
    Current Functions:
    1. Retrieve photos from gallery and analyse.

---
+++ app/src/main/java/com/peter/facedetective/FaceppDetect.java
@@ -0,0 +1,64 @@
+package com.peter.facedetective;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.facepp.error.FaceppParseException;
+import com.facepp.http.HttpRequests;
+import com.facepp.http.PostParameters;
+import com.facepp.result.FaceppResult;
+
+import java.io.ByteArrayOutputStream;
+
+/**
+ * Created by 韬 on 2015/8/20 0020.
+ */
+public class FaceppDetect {
+    public interface Callback{
+        void success(FaceppResult result);
+
+        void error(FaceppParseException exception);
+
+    }
+
+    public static void detect(final Bitmap bm, final Callback callback, final Context context){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    // 创建请求
+                    HttpRequests requests = new HttpRequests(Constant.KEY, Constant.SECRET, true);    // 不行就改成false
+
+                    Bitmap bmSmall = Bitmap.createBitmap(bm, 0, 0, bm.getWidth(), bm.getHeight());
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                    bmSmall.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+
+                    byte[] arrays = stream.toByteArray();
+
+                    PostParameters params = new PostParameters();
+                    params.setImg(arrays);
+                    FaceppResult jsonObject = requests.detectionDetect(params);
+
+                    //
+//                    Log.i("TAG", "This is to see whether it works or not");
+                    Log.i("TAG", jsonObject.toString());
+
+                    if(callback != null){
+                        callback.success(jsonObject);
+                    }
+                }
+                catch (FaceppParseException e){
+                    e.printStackTrace();
+                    if(callback == null){
+                        callback.error(e);
+                    }
+
+                    Toast.makeText(context, "Exception Found", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }).start();
+    }
+}
---
+++ app/src/main/java/com/peter/facedetective/MainActivity.java
@@ -0,0 +1,326 @@
+package com.peter.facedetective;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.net.Uri;
+import android.os.Message;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facepp.error.FaceppParseException;
+import com.facepp.result.FaceppResult;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final int PICK_CODE = 5221;
+    private ImageButton detect;
+    private Button getImage;
+    private ImageView photo;
+    private TextView count;
+    private FrameLayout waiting;
+    private ProgressBar ring;
+
+    private String currentPhotoStr;
+//    private String lastPhotoStr;
+
+    private Bitmap photoImage;
+    private Paint paint;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+        initEvents();
+
+        paint = new Paint();
+        setTitle(R.string.app_name);
+    }
+
+    private void initEvents() {
+        getImage.setOnClickListener(this);
+        detect.setOnClickListener(this);
+    }
+
+    private void initViews() {
+        detect = (ImageButton)findViewById(R.id.detect);
+        getImage = (Button)findViewById(R.id.getImage);
+        photo = (ImageView)findViewById(R.id.photo);
+        count = (TextView)findViewById(R.id.count);
+        waiting = (FrameLayout)findViewById(R.id.waiting);
+        ring = (ProgressBar)findViewById(R.id.ring);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.getImage:
+                setTitle(R.string.selectImage);
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, PICK_CODE);
+//                setTitle("Detect Faces");
+                break;
+
+            case R.id.detect:
+                if(currentPhotoStr != null && !currentPhotoStr.trim().equals("")){
+                    setTitle(R.string.detecting);
+                    waiting.setVisibility(View.VISIBLE);
+                    ring.setVisibility(View.VISIBLE);
+                    count.setVisibility(View.INVISIBLE);
+
+                }
+                else {
+                    Toast.makeText(this, R.string.noPhotoSelected, Toast.LENGTH_SHORT).show();
+                    break;
+                }
+
+                resizePhoto();
+
+                FaceppDetect.detect(photoImage, new FaceppDetect.Callback() {
+                    @Override
+                    public void success(FaceppResult result) {
+                        Message msg = Message.obtain();
+                        msg.what = MSG_SUCCESS;
+                        msg.obj = result;
+                        handler.sendMessage(msg);
+                    }
+
+                    @Override
+                    public void error(FaceppParseException exception) {
+                        Message msg = Message.obtain();
+                        msg.what = MSG_ERROR;
+                        msg.obj = exception.getErrorMessage();
+                        handler.sendMessage(msg);
+//                        setTitle(R.string.app_name);
+                    }
+                }, MainActivity.this);
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == PICK_CODE){
+            if (data != null){
+                Uri uri = data.getData();
+                Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+                cursor.moveToFirst();
+
+                int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+                currentPhotoStr = cursor.getString(idx);
+                cursor.close();
+
+                resizePhoto();
+
+                photo.setImageBitmap(photoImage);
+                count.setText(R.string.detectWithArrow);
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    // 压缩图片，防止过大
+    private void resizePhoto(){
+        BitmapFactory.Options option = new BitmapFactory.Options();
+        option.inJustDecodeBounds = true;
+
+        BitmapFactory.decodeFile(currentPhotoStr, option);
+
+        double ratio = Math.max(option.outWidth * 1.0d / 1024f, option.outHeight * 1.0d / 1024f);
+
+        option.inSampleSize = (int)Math.ceil(ratio);
+        option.inJustDecodeBounds = false;
+
+        // 第一次将从Gallery中获取的图片加给photoImage
+        photoImage = BitmapFactory.decodeFile(currentPhotoStr, option);
+    }
+
+    private static final int MSG_SUCCESS = 0x111;
+    private static final int MSG_ERROR = 0x112;
+
+    android.os.Handler handler = new android.os.Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case MSG_SUCCESS:
+                    waiting.setVisibility(View.INVISIBLE);           // INVISIBLE 还是 GONE 要注意
+                    ring.setVisibility(View.INVISIBLE);
+                    count.setVisibility(View.VISIBLE);
+                    FaceppResult result = (FaceppResult)msg.obj;
+
+                    // 解析JsonObject，绘制脸部框
+                    prepareResultBitmap(result);
+
+                    photo.setImageBitmap(photoImage);
+
+                    break;
+
+                case MSG_ERROR:
+                    waiting.setVisibility(View.GONE);
+                    ring.setVisibility(View.INVISIBLE);
+                    count.setVisibility(View.VISIBLE);
+                    String errorMsg = msg.obj.toString();
+
+                    if(TextUtils.isEmpty(errorMsg)){
+                        count.setText("Error");
+                    }
+                    else {
+                        count.setText(errorMsg);
+                    }
+
+                    break;
+            }
+            super.handleMessage(msg);
+        }
+    };
+
+    private void prepareResultBitmap(FaceppResult result) {
+        // 根据原图创建一个新的空画板
+        Bitmap bitmap = Bitmap.createBitmap(photoImage.getWidth(), photoImage.getHeight(), photoImage.getConfig());              // 此bitmap为最终的用来呈现给用户的图像
+        Canvas canvas = new Canvas(bitmap);                // 把bitmap当作一幅canvas作画
+
+        //将原图画入
+        canvas.drawBitmap(photoImage, 0, 0, null);
+
+        try{
+            // 转换成JSONObject
+            JSONObject rs = new JSONObject(result.toString());
+            JSONArray faces = rs.getJSONArray("face");
+
+            setTitle(R.string.app_name);
+
+            int faceCount = faces.length();
+            count.setText("No: " + faceCount);
+
+            for(int i = 0; i < faceCount; i++){
+                // 拿到单独的face对象
+                JSONObject face = faces.getJSONObject(i);
+                JSONObject posObj = face.getJSONObject("position");
+
+                /*
+                 代表图片高度/宽度的百分比
+                 中心点的位置
+                  */
+                float x = (float) posObj.getJSONObject("center").getDouble("x");
+                float y = (float) posObj.getJSONObject("center").getDouble("y");
+
+                float w = (float) posObj.getDouble("width");
+                float h = (float) posObj.getDouble("height");
+
+                // 百分比转换为实际像素值
+                x = x / 100 * bitmap.getWidth();
+                y = y / 100 * bitmap.getHeight();
+                w = w / 100 * bitmap.getWidth();
+                h = h / 100 * bitmap.getHeight();
+
+                paint.setColor(0xffffffff);
+                paint.setStrokeWidth(5);
+
+                // 画脸部区域的box
+                // 开始点的横纵坐标，结束点的横纵坐标
+                canvas.drawLine(x - w/2, y - h/2, x - w/2, y + h/2, paint);
+                canvas.drawLine(x - w/2, y + h/2, x + w/2, y + h/2, paint);
+                canvas.drawLine(x + w/2, y - h/2, x + w/2, y + h/2, paint);
+                canvas.drawLine(x - w/2, y - h/2, x + w/2, y - h/2, paint);
+
+                // 年龄和性别
+                int age = face.getJSONObject("attribute").getJSONObject("age").getInt("value");
+                String gender = face.getJSONObject("attribute").getJSONObject("gender").getString("value");
+
+                // 生成合适的用TextView控件表示的年龄和性别框
+                Bitmap ageBitmap = buildAgeBitmap(age, "Male".equals(gender));
+
+                // 根据图像大小缩放
+                int ageWidth = ageBitmap.getWidth();
+                int ageHeight = ageBitmap.getHeight();
+
+                if(bitmap.getWidth() < photoImage.getWidth() && bitmap.getHeight() < photoImage.getHeight()){
+                    float ratio = Math.max(bitmap.getWidth() * 1.0f / photo.getWidth(), bitmap.getHeight() * 1.0f / photo.getHeight());
+                    ageBitmap = Bitmap.createScaledBitmap(ageBitmap, (int)(ageWidth * ratio), (int)(ageHeight * ratio), false);
+                }
+
+                // 将年龄与性别框绘制到指定的位置
+                canvas.drawBitmap(ageBitmap, x - ageBitmap.getWidth() / 2, y - h/2 - ageBitmap.getHeight(), null);
+
+                photoImage = bitmap;
+
+                setTitle(R.string.app_name);
+            }
+        }
+        catch (Exception e){
+
+        }
+
+    }
+
+    private Bitmap buildAgeBitmap(int age, boolean isMale) {
+//        TextView ageAndGender = (TextView)findViewById(R.id.ageAndGender);
+        TextView ageAndGender = (TextView)waiting.findViewById(R.id.ageAndGender);
+        ageAndGender.setText(age + "");
+        if (isMale){
+            ageAndGender.setCompoundDrawablesWithIntrinsicBounds(getResources().getDrawable(R.drawable.male), null, null, null);           // 左上右下
+        }
+        else {
+            ageAndGender.setCompoundDrawablesWithIntrinsicBounds(getResources().getDrawable(R.drawable.female), null, null, null);
+        }
+        ageAndGender.setDrawingCacheEnabled(true);
+
+        /*
+        此段代码是防止DrawingCache为空
+         */
+        ageAndGender.measure(
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        ageAndGender.layout(0, 0, ageAndGender.getMeasuredWidth(),
+                ageAndGender.getMeasuredHeight());
+        ageAndGender.buildDrawingCache();
+
+        Bitmap bitmap = Bitmap.createBitmap(ageAndGender.getDrawingCache());
+        ageAndGender.destroyDrawingCache();
+
+        return bitmap;
+    }
+}
