151bfcd3250514fa791d4f8aa9cf7c3d78af0f9f
TPeterW <peterwangtao0@hotmail.com>
2017-05-17 14:25:06 -0400

    Let me try try

--- app/src/androidTest/java/com/peter/facedetective/InstrumentedTest.java
+++ app/src/androidTest/java/com/peter/facedetective/InstrumentedTest.java
@@ -1,6 +1,5 @@
 package com.peter.facedetective;
 
-import android.app.Application;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
@@ -8,17 +7,20 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 /**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
 public class InstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
+        // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.tpwang.testproject", appContext.getPackageName());
+        assertEquals("com.peter.facedetective", appContext.getPackageName());
     }
 }
